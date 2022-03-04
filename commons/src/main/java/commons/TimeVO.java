f54f64ce51e590e60b7d3db694753a30523a8957
huangzhongjun@sisyphe.com.cn <huangzhongjun@sisyphe.com.cn>
2022-03-04 10:56:30 +0800

    add time format

--- commons/src/main/java/commons/TimeVO.java
+++ commons/src/main/java/commons/TimeVO.java
@@ -2,11 +2,14 @@ package commons;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
 public class TimeVO {
 
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private LocalDateTime time;
 
 	private String message;
--- web/src/main/java/web/user/vo/UserVO.java
+++ web/src/main/java/web/user/vo/UserVO.java
@@ -2,6 +2,8 @@ package web.user.vo;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.Data;
 
 @Data
@@ -9,5 +11,6 @@ public class UserVO {
 
 	private String name;
 
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
 	private LocalDateTime time;
 }
